4d77f8018bc7b9a5f43a5d5978dfa03f08165950
veslorand <61459386+veslorand@users.noreply.github.com>
2021-02-04 17:55:52 +0100

    Add /minifigs path to list Minifigs

--- src/context/LegoMinifigsContext.js
+++ src/context/LegoMinifigsContext.js
@@ -4,11 +4,12 @@ import {getLegoMinifigs} from '../utils/Http'
 export const LegoMinifigsContext = createContext(undefined);
 
 export const LegoMinifigsProvider = (props) => {
-    const [legoMinifigs, setLegoMinifigs] = useState([])
+    const [legoMinifigs, setLegoMinifigs] = useState([]);
+
     useEffect(() => {
         getLegoMinifigs()
             .then(response => {
-                setLegoMinifigs([response.data.results]);
+                setLegoMinifigs([...response.data.results]);
             })
     }, [])
 
