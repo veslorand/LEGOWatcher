a32910e10590f080fdc87b395607d1e648143289
veslorand <61459386+veslorand@users.noreply.github.com>
2021-02-05 01:06:31 +0100

    Rebuild LegoSetElement.js card

--- src/components/legosets/LegoSetElement.js
+++ src/components/legosets/LegoSetElement.js
@@ -1,63 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import theme from "../../theme";
+import {CardActionArea, CardActions, CardContent, CardMedia, makeStyles} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {AddLegoToWishlist} from "../../utils/axios";
 
-const Sprite = styled.img`
-  width: 22em;
-  height: 22em;
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: black;
-`;
 
 function LegoSetElement(props) {
     const legoSet = props.legoset;
+    const classes = useStyles();
 
     return (
-        <div className="col-md-4 col-sm-6 mb-5">
-            <StyledLink
-                to={`/sets/${LegoSetElement}`}
-                style={elementStyle}
-            >
-                <Card className="card">
-                    <div className="card-header" style={cardHeader}>
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={legoSet.set_img_url}
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
                         {legoSet.name}
-                    </div>
-                    <div className="card-body mx-auto">
-                        <Sprite
-                            className="card-img-center rounded mx-auto mt-2"
-                            src={legoSet.set_img_url}
-                        />
-                    </div>
-                </Card>
-            </StyledLink>
-        </div>
+                    </Typography>
+                    <Typography variant="body2" color="textPrimary" component="p">
+                        {legoSet.parts}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="large" color="secondary" backgroundColor={'blue'} onClick={AddLegoToWishlist(props.legoMinifig)}>
+                    Add to WishList
+                </Button>
+                <Button size="small" color="secondary">
+                    Add to OwnedList
+                </Button>
+            </CardActions>
+        </Card>
     );
 
 }
-const elementStyle = {
-    textDecoration: "none",
-};
-
-const cardHeader = {
-    fontWeight: "bold",
-    color: "black",
-    backgroundColor: `${theme ? "#ffe000" : "black"}`,
-};
 
 const Card = styled.div`
-  
+
   font-weight: bold;
-  color: ${theme ? "#000000" : "#ff0000"};
+  color: ${theme ? "#ffffff" : "#ff0000"};
   background-color: ${theme ? "#ff0000" : "#333"};
   box-shadow: 0 1px 3px;
   transition: all 0.5s;
+
   &:hover {
     box-shadow: 0 7px 14px;
   }
 `;
+
+const useStyles = makeStyles({
+    root: {
+        width: 300,
+    },
+    media: {
+        height: 300,
+    },
+});
 
 export default LegoSetElement;
